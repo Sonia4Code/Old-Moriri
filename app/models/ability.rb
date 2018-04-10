e49b1a30d2107178dbcf2abc75f80f99a4e96741
SoniaMakhetha <sonia.makhetha@gmail.com>
2018-04-10 18:29:19 +0800

    fixed slight issues with ability model

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,24 +1,23 @@
 class Ability
   include CanCan::Ability
-
-  def initialize(user)
-    user ||= User.new # guest user
-    
-    if user.role? :super_admin
-      can :manage, :all
-    else
-      can :read, :all
-      # can :create, Review
-      # can :update, Review do |review|
-      #   review.try(:user) == user || user.role?(:client)
+    def initialize(user)
+      user ||= User.new # guest user
+      
+      if user.role? :super_admin
+        can :manage, :all
+      else
+        can :read, :all
+        # can :create, Review
+        # can :update, Review do |review|
+        #   review.try(:user) == user || user.role?(:client)
       end
       if user.role?(:owner)
         can :create, Listing
         can :update, Listing do |listing|
-          listing.try(:user) == user
+        listing.try(:user) == user
         end
       end
+     end
     end
   end
-
 end
