84fa4935baf3b1422a7ad0205102b3a11f40d679
SoniaMakhetha <sonia.makhetha@gmail.com>
2018-04-26 16:46:46 +0800

    added fb login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,16 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], 
+  
+  provider :facebook, ENV['FACEBOOK_APP_KEY'], ENV['FACEBOOK_APP_SECRET'] 
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
     scope: 'email, profile, plus.login',
     provider_ignores_state: true,
     prompt: 'select_account',
     image_aspect_ratio: 'square',
     image_size: 100,
     skip_jwt: true
+  
+
+
 end
 
 
