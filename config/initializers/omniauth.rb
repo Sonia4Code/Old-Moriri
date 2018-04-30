db79e35d94006e66b1b34a08e05512611bbc6ede
SoniaMakhetha <sonia.makhetha@gmail.com>
2018-04-30 15:53:30 +0800

    modifying facebook login scope

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,13 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   
-  provider :facebook, ENV['FACEBOOK_APP_KEY'], ENV['FACEBOOK_APP_SECRET'] 
+  provider :facebook, ENV['FACEBOOK_APP_KEY'], ENV['FACEBOOK_APP_SECRET'],
+    scope: 'profile',
+    provider_ignores_state: true,
+    prompt: 'select_account',
+    image_aspect_ratio: 'square',
+    image_size: 100,
+    skip_jwt: true
+
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
     scope: 'email, profile, plus.login',
     provider_ignores_state: true,
